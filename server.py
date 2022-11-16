6d55178ee2b8d8d061cabb3141474732d7c97df5
Joel Forman <joel.forman@berkeley.edu>
2022-11-16 02:30:46 -0800

    Finished front end for static Run and About page

--- server.py
+++ server.py
@@ -4,9 +4,13 @@ from flask_bootstrap import Bootstrap
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
