2c59e9fc2daa11b05146070f46812abee0e5c200
Joel Forman <joel.forman@berkeley.edu>
2023-02-23 15:55:38 -0800

    Finished calculation of PSD ratios from edf files

--- server.py
+++ server.py
@@ -17,86 +17,113 @@ def index():
 def about():
     return render_template('about.html')
 
-def power_spectral_density(data, srate):
-    data = np.array(data)
-    f, Pxx_den = signal.welch(data, srate, nperseg=1024)
-    return f, Pxx_den
-
 
 def readEDF(filepath, filldisrupt = False):
     #open the file using pyedflib
     file = pyedflib.EdfReader(filepath)
     signal_labels = file.getSignalLabels()
     print('signal_labels: ', signal_labels)
+
+    channel1 = file.readSignal(0)
+    channel2 = file.readSignal(1)
+
     #get the number of hours
     srate = file.getSampleFrequency(0)
-    total_seconds = file.getNSamples()[0]
-    num_hrs = total_seconds/(srate*3600)
-    #parse the data into 24 hours and 10 minutes intervals
-    num_hrs = int(num_hrs)
-    thresh = int(total_seconds/(num_hrs*6))
-    #initialize columns to hold the average for the first EEG electrode
-    my_avg_3_eeg1 = np.empty(thresh)
-    my_avg_10_eeg1 = np.empty(thresh)
-    #initialize columns to hold the average for the second EEG electrode
-    my_avg_3_eeg2 = np.empty(thresh)
-    my_avg_10_eeg2 = np.empty(thresh)
-    #initialize two variables for holding mean values
-    final_avg_1 = np.empty(1)
-    final_avg_2 = np.empty(1)
-    #go through all the data for every 10 minute window
-    for i in range(0, num_hrs*6):
-        #read data from the file
-        data = file.readSignal(i)
-        if filldisrupt:
-            idx = np.where(data[0] == 0.00045)[0]
-            data = np.delete(data, idx, axis=1)
-        if len(data) != 0:
-            #compute power spectral density
-            psd = power_spectral_density(data, srate)
-            #find range
-            hold_3_hz = [x for x in psd if 3 <= x[0] <= 3.5]
-            hold_10_hz = [x for x in psd if 9.5 <= x[0] <= 10]
-            #find the mean of hold3Hz and hold10Hz for the first electrode
-            my_avg_3_eeg1[i] = np.mean([x[1] for x in hold_3_hz])
-            my_avg_10_eeg1[i] = np.mean([x[1] for x in hold_10_hz])
-            #find the mean of hold3Hz and hold10Hz for the second electrode
-            my_avg_3_eeg2[i] = np.mean([x[1] for x in hold_3_hz])
-            my_avg_10_eeg2[i] = np.mean([x[1] for x in hold_10_hz])
-    #compute final averages
-    final_avg_1 = np.mean(np.concatenate((my_avg_3_eeg1, my_avg_10_eeg1)))
-    final_avg_2 = np.mean(np.concatenate((my_avg_3_eeg2, my_avg_10_eeg2)))
-    file._close()
-    return final_avg_1, final_avg_2
+    total_seconds = file.getFileDuration()
+    # print('total_seconds: ', total_seconds)
+    # print('length of first signal: ', len(channel1))
+    # print('length of second signal: ', len(channel2))
+    # print('srate: ', srate)
+    # print('srate * total seconds: ', srate * total_seconds)
+    num_hrs = total_seconds/3600
+    num_ten_min_intervals = num_hrs * 6
+    samples_per_ten_mins = int(len(channel1) / num_ten_min_intervals)
+    print('length of samples_per_ten_mins: ', samples_per_ten_mins)
 
-def post_process_data(final_avg_1, final_avg_2, filldis=False):
-    #Initialize variables for holding results of computation
-    avgavg1 = []
-    avgavg2 = []
-    normavg1 = []
-    normavg2 = []
-    #find the average of ratios for every hour
-    numhm = len(final_avg_1)//6 - 1
-    for k in range(0, numhm+1):
-        #find the average for every hour
-        valVal1 = final_avg_1[(6*(k)):(6*(k+1))]
-        valVal2 = final_avg_2[(6*(k)):(6*(k+1))]
-        avgavg1.append(sum(valVal1)/len(valVal1))
-        avgavg2.append(sum(valVal2)/len(valVal2))
-    #create a dataframe with time and EEG1 and EEG2 columns
-    coolEDFTable = pd.DataFrame(columns=["time", "EEG1", "EEG2"])
-    coolEDFTable["time"] = range(1, 25)
-    coolEDFTable["EEG1"] = avgavg1 + [float('nan')]*(24-len(avgavg1))
-    coolEDFTable["EEG2"] = avgavg2 + [float('nan')]*(24-len(avgavg2))
-    coolEDFTable = coolEDFTable.dropna()
-    coolEDFTableF = coolEDFTable.iloc[:12]
-    coolEDFTableS = coolEDFTable.iloc[12:]
-    if filldis:
-        if len(coolEDFTableF.dropna()) >= 3:
-            coolEDFTableF = coolEDFTableF.interpolate()
-        if len(coolEDFTableS.dropna()) >= 3:
-            coolEDFTableS = coolEDFTableS.interpolate()
-    return coolEDFTableF, coolEDFTableS
+    ten_min_ratios_ch1 = np.array([])
+    ten_min_ratios_ch2 = np.array([])
+
+    for i in range(0, int(num_ten_min_intervals)):
+        ten_min_signals_ch1 = channel1[i*samples_per_ten_mins:(i*samples_per_ten_mins + samples_per_ten_mins)]
+        ten_min_signals_ch2 = channel2[i*samples_per_ten_mins:(i*samples_per_ten_mins + samples_per_ten_mins)]
+        print(f'channel 1 interval {i} length: {len(ten_min_signals_ch1)}')
+        print(f'channel 2 interval {i} length: {len(ten_min_signals_ch2)}')
+
+        ten_min_psd_ch1 = signal.welch(ten_min_signals_ch1, len(ten_min_signals_ch1) / 600)[1]
+        ten_min_psd_ch2 = signal.welch(ten_min_signals_ch2, len(ten_min_signals_ch2) / 600)[1]
+
+        #print(f'psd of 10 min interval of channel 1: {ten_min_psd_ch1}')
+        #print(f'psd of 10 min interval of channel 2: {ten_min_psd_ch2}')
+
+
+        # filter by frequencies
+
+        filtered_psd_3hz_ch1 = np.array([x for x in ten_min_psd_ch1 if 3 <= x <= 3.5])
+        filtered_psd_3hz_ch2 = np.array([x for x in ten_min_psd_ch2 if 3 <= x <= 3.5])
+        print(f'filtered_psd_3hz_ch1: {filtered_psd_3hz_ch1}')
+        print(f'filtered_psd_3hz_ch2: {filtered_psd_3hz_ch2}')
+
+        filtered_psd_10hz_ch1 = np.array([x for x in ten_min_psd_ch1 if 9.5 <= x <= 10])
+        filtered_psd_10hz_ch2 = np.array([x for x in ten_min_psd_ch2 if 9.5 <= x <= 10])
+        print(f'filtered_psd_10hz_ch1: {filtered_psd_10hz_ch1}')
+        print(f'filtered_psd_10hz_ch2: {filtered_psd_10hz_ch2}')
+
+        # take averages
+
+        avg_3hz_psd_ch1 = np.mean(filtered_psd_3hz_ch1)
+        avg_3hz_psd_ch2 = np.mean(filtered_psd_3hz_ch2)
+        print(f'mean 3hz psd ch1: {avg_3hz_psd_ch1}')
+        print(f'mean 3hz psd ch2: {avg_3hz_psd_ch2}')
+
+        avg_10hz_psd_ch1 = np.mean(filtered_psd_10hz_ch1)
+        avg_10hz_psd_ch2 = np.mean(filtered_psd_10hz_ch2)
+        print(f'mean 10hz psd ch1: {avg_10hz_psd_ch1}')
+        print(f'mean 10hz psd ch2: {avg_10hz_psd_ch2}')
+
+        ten_min_ratios_ch1 = np.append(ten_min_ratios_ch1, avg_10hz_psd_ch1/avg_3hz_psd_ch1)
+        ten_min_ratios_ch2 = np.append(ten_min_ratios_ch2, avg_10hz_psd_ch2/avg_3hz_psd_ch2)
+
+    print(f'all ten min rations channel1: {ten_min_ratios_ch1}')
+    print(f'all ten min rations channel2: {ten_min_ratios_ch2}')
+
+    final_hour_ratio_avgs_ch1 = np.array([])
+    final_hour_ratio_avgs_ch2 = np.array([])    
+
+    for i in range(len(ten_min_ratios_ch1) // 6):
+        sum = 0
+        num_total = 0
+        for j in range(i*6, (i*6 + 6)):
+            if not np.isnan(ten_min_ratios_ch1[j]):
+                sum += ten_min_ratios_ch1[j]
+                num_total += 1
+        if num_total == 0:
+            avg = 0
+        else:
+            avg = sum / num_total
+        print(f'avg ch1: {avg}')
+        final_hour_ratio_avgs_ch1 = np.append(final_hour_ratio_avgs_ch1, avg)
+
+    for i in range(len(ten_min_ratios_ch2) // 6):
+        sum = 0
+        num_total = 0
+        for j in range(i*6, (i*6 + 6)):
+            if not np.isnan(ten_min_ratios_ch2[j]):
+                sum += ten_min_ratios_ch2[j]
+                num_total += 1
+        if num_total == 0:
+            avg = 0
+        else:
+            avg = sum / num_total
+        final_hour_ratio_avgs_ch2 = np.append(final_hour_ratio_avgs_ch2, avg)
+
+    print(f'final hour by hour ratios ch1: {final_hour_ratio_avgs_ch1}')
+    print(f'final hour by hour ratios ch2: {final_hour_ratio_avgs_ch2}')
+
+    # standardize
+
+    print(f'standardized final hour by hour ratios ch1: {(final_hour_ratio_avgs_ch1 - np.mean(final_hour_ratio_avgs_ch1)) / np.std(final_hour_ratio_avgs_ch1)}')
+    print(f'standardized final hour by hour ratios ch2: {(final_hour_ratio_avgs_ch2 - np.mean(final_hour_ratio_avgs_ch2)) / np.std(final_hour_ratio_avgs_ch2)}')
+
 
 @app.route('/analysis.html', methods=['GET', 'POST'])
 def analysis():
