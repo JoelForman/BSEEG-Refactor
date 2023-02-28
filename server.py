db2b0a5a59b4102300da413a41cadd3b2b6793bc
Joel Forman <joel.forman@berkeley.edu>
2023-02-28 14:52:31 -0800

    Implemented the ability for the generated python numpy arrays of values to be passed to ChartJS

--- server.py
+++ server.py
@@ -46,42 +46,42 @@ def readEDF(filepath, filldisrupt = False):
     for i in range(0, int(num_ten_min_intervals)):
         ten_min_signals_ch1 = channel1[i*samples_per_ten_mins:(i*samples_per_ten_mins + samples_per_ten_mins)]
         ten_min_signals_ch2 = channel2[i*samples_per_ten_mins:(i*samples_per_ten_mins + samples_per_ten_mins)]
-        print(f'channel 1 interval {i} length: {len(ten_min_signals_ch1)}')
-        print(f'channel 2 interval {i} length: {len(ten_min_signals_ch2)}')
+        # print(f'channel 1 interval {i} length: {len(ten_min_signals_ch1)}')
+        # print(f'channel 2 interval {i} length: {len(ten_min_signals_ch2)}')
 
         ten_min_psd_ch1 = signal.welch(ten_min_signals_ch1, len(ten_min_signals_ch1) / 600)[1]
         ten_min_psd_ch2 = signal.welch(ten_min_signals_ch2, len(ten_min_signals_ch2) / 600)[1]
 
-        #print(f'psd of 10 min interval of channel 1: {ten_min_psd_ch1}')
-        #print(f'psd of 10 min interval of channel 2: {ten_min_psd_ch2}')
+        # print(f'len of psd of 10 min interval of channel 1: {len(ten_min_psd_ch1)}')
+        # print(f'len of psd of 10 min interval of channel 2: {len(ten_min_psd_ch2)}')
 
 
         # filter by frequencies
 
         filtered_psd_3hz_ch1 = np.array([x for x in ten_min_psd_ch1 if 3 <= x <= 3.5])
         filtered_psd_3hz_ch2 = np.array([x for x in ten_min_psd_ch2 if 3 <= x <= 3.5])
-        print(f'filtered_psd_3hz_ch1: {filtered_psd_3hz_ch1}')
-        print(f'filtered_psd_3hz_ch2: {filtered_psd_3hz_ch2}')
+        # print(f'filtered_psd_3hz_ch1: {filtered_psd_3hz_ch1}')
+        # print(f'filtered_psd_3hz_ch2: {filtered_psd_3hz_ch2}')
 
         filtered_psd_10hz_ch1 = np.array([x for x in ten_min_psd_ch1 if 9.5 <= x <= 10])
         filtered_psd_10hz_ch2 = np.array([x for x in ten_min_psd_ch2 if 9.5 <= x <= 10])
-        print(f'filtered_psd_10hz_ch1: {filtered_psd_10hz_ch1}')
-        print(f'filtered_psd_10hz_ch2: {filtered_psd_10hz_ch2}')
+        # print(f'filtered_psd_10hz_ch1: {filtered_psd_10hz_ch1}')
+        # print(f'filtered_psd_10hz_ch2: {filtered_psd_10hz_ch2}')
 
         # take averages
 
         avg_3hz_psd_ch1 = np.mean(filtered_psd_3hz_ch1)
         avg_3hz_psd_ch2 = np.mean(filtered_psd_3hz_ch2)
-        print(f'mean 3hz psd ch1: {avg_3hz_psd_ch1}')
-        print(f'mean 3hz psd ch2: {avg_3hz_psd_ch2}')
+        # print(f'mean 3hz psd ch1: {avg_3hz_psd_ch1}')
+        # print(f'mean 3hz psd ch2: {avg_3hz_psd_ch2}')
 
         avg_10hz_psd_ch1 = np.mean(filtered_psd_10hz_ch1)
         avg_10hz_psd_ch2 = np.mean(filtered_psd_10hz_ch2)
-        print(f'mean 10hz psd ch1: {avg_10hz_psd_ch1}')
-        print(f'mean 10hz psd ch2: {avg_10hz_psd_ch2}')
+        # print(f'mean 10hz psd ch1: {avg_10hz_psd_ch1}')
+        # print(f'mean 10hz psd ch2: {avg_10hz_psd_ch2}')
 
-        ten_min_ratios_ch1 = np.append(ten_min_ratios_ch1, avg_10hz_psd_ch1/avg_3hz_psd_ch1)
-        ten_min_ratios_ch2 = np.append(ten_min_ratios_ch2, avg_10hz_psd_ch2/avg_3hz_psd_ch2)
+        ten_min_ratios_ch1 = np.append(ten_min_ratios_ch1, avg_3hz_psd_ch1/avg_10hz_psd_ch1)
+        ten_min_ratios_ch2 = np.append(ten_min_ratios_ch2, avg_3hz_psd_ch2/avg_10hz_psd_ch2)
 
     print(f'all ten min rations channel1: {ten_min_ratios_ch1}')
     print(f'all ten min rations channel2: {ten_min_ratios_ch2}')
@@ -100,7 +100,6 @@ def readEDF(filepath, filldisrupt = False):
             avg = 0
         else:
             avg = sum / num_total
-        print(f'avg ch1: {avg}')
         final_hour_ratio_avgs_ch1 = np.append(final_hour_ratio_avgs_ch1, avg)
 
     for i in range(len(ten_min_ratios_ch2) // 6):
@@ -124,9 +123,12 @@ def readEDF(filepath, filldisrupt = False):
     print(f'standardized final hour by hour ratios ch1: {(final_hour_ratio_avgs_ch1 - np.mean(final_hour_ratio_avgs_ch1)) / np.std(final_hour_ratio_avgs_ch1)}')
     print(f'standardized final hour by hour ratios ch2: {(final_hour_ratio_avgs_ch2 - np.mean(final_hour_ratio_avgs_ch2)) / np.std(final_hour_ratio_avgs_ch2)}')
 
+    #return (final_hour_ratio_avgs_ch1 - np.mean(final_hour_ratio_avgs_ch1)) / np.std(final_hour_ratio_avgs_ch1), (final_hour_ratio_avgs_ch2 - np.mean(final_hour_ratio_avgs_ch2)) / np.std(final_hour_ratio_avgs_ch2)
+    return final_hour_ratio_avgs_ch1, final_hour_ratio_avgs_ch2
 
 @app.route('/analysis.html', methods=['GET', 'POST'])
 def analysis():
+    final_avg1 = final_avg2 = []
     if request.method == 'POST':
         # Get the uploaded file
         file = request.files['file']
@@ -140,15 +142,15 @@ def analysis():
             print(file)
 
             final_avg1, final_avg2 = readEDF('uploaded_file.edf')
-            print('final_avg1: ', final_avg1)
-            print('final_avg2: ', final_avg2)
+            # print('final_avg1: ', final_avg1)
+            # print('final_avg2: ', final_avg2)
             #coolEDFTableF, coolEDFTableS = post_process_data(final_avg1, final_avg2)
             #print("First table: ", coolEDFTableF)
             #print("Second table: ", coolEDFTableS)
         else:
             # Return an error message
             return 'Invalid file format. Only .edf files are allowed.'
-    return render_template('analysis.html')
+    return render_template('analysis.html', final_avg1=final_avg1.tolist(), final_avg2=final_avg2.tolist())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/analysis2.js
+++ static/js/analysis2.js
@@ -1,14 +1,30 @@
+console.log("Successfully passed final_avg1...");
+console.log(final_avg1);
+console.log("SUccessfully passed final_avg2...");
+console.log(final_avg2);
+
+final_avg1 = final_avg1.map(function(value) {
+  return value === 0 ? null : value;
+});
+
+final_avg2 = final_avg2.map(function(value) {
+  return value === 0 ? null : value;
+});
+
+// old dummy data: [2,2.5,2.6,1.9,2.7,2.3,3.1,3.5,3.3,3.5,3.9,4.3,3.9,3.4,3.0,2.3,3.1,3.5,3.3,3.5,3.9,2.3,3.1,3.5,3.3,]
+// old dummy data 2: data: [2.5,3.3,3.5,3.9,2.3,2,2.5,3.9,3.4,3.0,2.3,3.1,3.5,3.1,3.5,3.3,3.5,3.9,4.3,3.3,3.5,3.9,2.3,3.1,3.5,3.3,]
+
 new Chart(document.getElementById("line-chart"), { 
     type: 'line',
     data: {
-      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15.,16,17,18,19,20,21,22,23,24],
+      labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15.,16,17,18,19,20,21,22,23],
       datasets: [{ 
-          data: [2,2.5,2.6,1.9,2.7,2.3,3.1,3.5,3.3,3.5,3.9,4.3,3.9,3.4,3.0,2.3,3.1,3.5,3.3,3.5,3.9,2.3,3.1,3.5,3.3,],
+          data: final_avg1,
           label: "EEG1",
           borderColor: "#3e95cd",
           fill: false
         }, { 
-          data: [2.5,3.3,3.5,3.9,2.3,2,2.5,3.9,3.4,3.0,2.3,3.1,3.5,3.1,3.5,3.3,3.5,3.9,4.3,3.3,3.5,3.9,2.3,3.1,3.5,3.3,],
+          data: final_avg2,
           label: "EEG2",
           borderColor: "#8e5ea2",
           fill: false
